d237253b1b022eec0507c553d78ddb6cd8926e68
Nomasonto Ndlovu <ndlovunomasonto51@gmail.com>
2026-09-21 12:43:07 +0200

    Add files via upload

---
+++ funchat.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class funchat {
+   private static int messagecount=0;
+    public static void startchart(String sender){
+        Scanner scanner=new Scanner(System.in);
+        while (true){
+
+            System.out.println("welcome to funchart");
+            System.out.println("select option");
+            System.out.println("1; select chat options");
+            System.out.println("2;send message");
+            System.out.println("3 quit");
+System.out.println(" enter your choice");
+int choice= scanner.nextInt();
+String receiver="";
+scanner.nextLine();
+switch (choice){
+    case 2:System.out.println("you have selected send message");
+    do {
+        System.out.println("enter receiver number(must start with +27 and have exactly 12 numbers)");
+        receiver=scanner.nextLine();
+    }while (receiver.startsWith("+27")&& receiver.length()==12);
+    System.out.println("enter your message it must be 250 characters or less");
+    String message=scanner.nextLine();
+            if (message.length()>250) {
+                System.out.println("ensure that your message is 250 charecters of less");
+                return;
+
+            }
+            String messageid =generatemessageid();
+            int currentmessagecount=++messagecount;
+            String messageHash=genertemassagehash(messageid,messagecount, message);
+            System.out.println("messagehash"+messageHash);
+            System.out.println("choose option");
+            System.out.println("send message");
+            System.out.println("store send message");
+            System.out.println("delete message");
+            int subchoice= scanner.nextInt();
+            switch (subchoice){
+                case 1:System.out.println("messagesent");
+                break;
+                case 2:System.out.println("message deleted");
+                break;
+                case 3:storemessage(messageid,receiver,message,messageHash);
+                break;
+                default:
+                    System.out.println("invalid option");
+            }
+            break;
+
+
+
+
+    case 3: System.out.println("quitting");
+
+
+    return;
+}
+
+
+        }
+    }
+
+}
