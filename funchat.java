9b2401e6bbf21f99c29dc25012076fe7297b7980
Nomasonto Ndlovu <ndlovunomasonto51@gmail.com>
2026-09-23 09:53:40 +0200

    Implement message ID generation and storage
    Added message ID generation and message storage functionality.

--- funchat.java
+++ funchat.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class funchat {
@@ -20,7 +24,7 @@ switch (choice){
     do {
         System.out.println("enter receiver number(must start with +27 and have exactly 12 numbers)");
         receiver=scanner.nextLine();
-    }while (receiver.startsWith("+27")&& receiver.length()==12);
+    }while (receiver.startsWith("+27")&& receiver.length()<=12);
     System.out.println("enter your message it must be 250 characters or less");
     String message=scanner.nextLine();
             if (message.length()>250) {
@@ -56,10 +60,40 @@ switch (choice){
 
 
     return;
+    default:System.out.println("invalid choice choose 1,2,3");
 }
 
-
-        }
+}
     }
+    private static String generatemessageid(){
+        Random random=new Random();
+        String id ="";
+        for(int i =0;i<10;i++){
+            id+= random.nextInt(10);
+        }
+        return id;
+    }
+private static String genertemassagehash(String messageid,int count,String message){
+        String[] words=message.trim().split("\\s+");
+        String first=words.length>0? words[0]:"";
+        String last=words.length>1? words[words.length-1]:first;
+        return messageid.substring(0,2)+":"+count+":"+ first.toLowerCase()+last.toLowerCase();
+
+    }
+    private static void storemessage(String id,String receiver,String message,String hash){
+        try(FileWriter fileWriter=new FileWriter("stored message.txt",true)) {
+            fileWriter.write("messageid:"+id+"\n");
+            fileWriter.write("receiver:"+receiver+"\n");
+            fileWriter.write("message:"+message+"\n");
+            fileWriter.write("hash:"+hash+"\n");
+            fileWriter.write("-----\n");
+            System.out.println("the message has been stored successfully");
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
